ad27f69d083b77080ca8eb00f44759c9bbfcd815
arafat <arafat0164484@gmail.com>
2023-07-04 19:36:56 +0600

    time set for carousel

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const menuIcon = document.querySelector(".mobile-menu>i");
 menuIcon.addEventListener("click", function () {
   const nav = document.querySelector("header nav");
   nav.classList.add("active");
-  document.querySelector(".menu-background-shadwo").classList.add("d-block");
+  document.querySelector(".menu-background-shadwo").classList.add("full-opacity");
 });
 
 function closemenu() {
   const nav = document.querySelector("header nav");
   nav.classList.remove("active");
-  document.querySelector(".menu-background-shadwo").classList.remove("d-block");
+  document.querySelector(".menu-background-shadwo").classList.remove("full-opacity");
 }
 const closeIcon = document.querySelector("header .close");
 closeIcon.addEventListener("click", closemenu);
@@ -16,7 +16,7 @@ document
   .querySelector(".menu-background-shadwo")
   .addEventListener("click", closemenu);
 
-document.querySelector('[data-serial="1"]').classList.add("d-block");
+document.querySelector('[data-serial="1"]').classList.add("full-opacity");
 
 const dataSerials = [];
 
@@ -29,42 +29,50 @@ const min = dataSerials[0];
 const max = dataSerials[dataSerials.length - 1];
 console.log(min, max);
 
-document.querySelector("#next").addEventListener("click", function () {
-  const selectedImage = document.querySelector(".d-block");
+
+
+const nextImage = function () {
+  const selectedImage = document.querySelector(".full-opacity");
 
   const data = selectedImage.getAttribute("data-serial");
 
-  document.querySelector(`[data-serial="${data}"]`).classList.remove("d-block");
+  document.querySelector(`[data-serial="${data}"]`).classList.remove("full-opacity");
 
   if (Number(data) !== max) {
     const nextBanner = document.querySelector(
       `[data-serial="${Number(data) + 1}`
     );
 
-    nextBanner.classList.add("d-block");
+    nextBanner.classList.add("full-opacity");
   } else {
     const nextBanner = document.querySelector(`[data-serial="${min}"`);
 
-    nextBanner.classList.add("d-block");
+    nextBanner.classList.add("full-opacity");
   }
-});
+}
 
-document.querySelector("#prev").addEventListener("click", function () {
-  const selectedImage = document.querySelector(".d-block");
+const prevImage = function () {
+  const selectedImage = document.querySelector(".full-opacity");
 
   const data = selectedImage.getAttribute("data-serial");
 
-  document.querySelector(`[data-serial="${data}"]`).classList.remove("d-block");
+  document.querySelector(`[data-serial="${data}"]`).classList.remove("full-opacity");
 
   if (Number(data) !== min) {
     const nextBanner = document.querySelector(
       `[data-serial="${Number(data) - 1}`
     );
 
-    nextBanner.classList.add("d-block");
+    nextBanner.classList.add("full-opacity");
   } else {
     const nextBanner = document.querySelector(`[data-serial="${max}"`);
 
-    nextBanner.classList.add("d-block");
+    nextBanner.classList.add("full-opacity");
   }
-});
+}
+
+document.querySelector("#next").addEventListener("click", nextImage);
+
+document.querySelector("#prev").addEventListener("click", prevImage);
+
+setInterval(nextImage,6*1000)
